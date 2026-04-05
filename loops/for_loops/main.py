ea7390865c084c2bbd77c340e341812a52d38acb
jacquezeliseo-sketch <>
2026-04-05 23:05:10 +0000

    For Loops

---
+++ loops/for_loops/main.py
@@ -0,0 +1,8 @@
+prices = [12.99, 8.50, 15.75, 23.00, 7.25]
+#create a variable total and set it to 0
+total = 0
+#use a for loop to iterate through each price in the prices list
+for price in prices:
+
+    total += price
+print(total)
